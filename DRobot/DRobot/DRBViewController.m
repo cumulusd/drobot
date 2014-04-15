e79b66c1166bdc7c66492ab9cb3474a4906b9e30
Daniel B <dbradford@aircumulus.gateway.2wire.net>
2014-04-14 20:53:57 -0500

    Test method

--- DRobot/DRobot/DRBViewController.m
+++ DRobot/DRobot/DRBViewController.m
@@ -20,6 +20,11 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
+-(void)testing
+{
+    
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
